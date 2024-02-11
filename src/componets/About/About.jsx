92d74f424c8e592b8202c7e9a892679f79f08b71
bnayak <bnayak62.bn@gmail.com>
2024-02-12 03:28:15 +0530

    add all the changes

---
+++ src/componets/About/About.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { getImageUrl } from "./../../utils";
+import styles from"./About.module.css";
+
+const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="photo with laptop" className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}> 
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="server" />
+            <div className={styles.aboutItemText}>
+              <h3> Backend Developer</h3>
+              <p>
+                I'm a Backend developer with experience in building
+                Microservices applications with best practices.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="cursor" />
+            <div className={styles.aboutItemText}>
+              <h3> Frontend Developer</h3>
+              <p>
+                I have experience developing responsive frontend applications.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="server" />
+            <div className={styles.aboutItemText}>
+              <h3> Backend Developer</h3>
+              <p>
+                I have experience developing fast and optimized back-end Systems
+                and APIs.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default About;
