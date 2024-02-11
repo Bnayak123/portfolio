d8522556340945e5e006090b8d5a7177bc37cd24
bnayak <bnayak62.bn@gmail.com>
2024-02-11 19:24:04 +0530

    code commit to the gtit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import styles from "./App.module.css";
+import Hero from "./componets/Navbar/Hero";
+import Navbar from "./componets/Navbar/Navbar";
+
+function App() {
+ 
+  return (
+    <div className={styles.App}> 
+     <Navbar/>
+     <Hero/>
+    </div>
+  )
+}
+
+export default App
